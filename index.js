a4d649c050c4284e570bdfcc1257a64bb8b12897
Kelly Williams <105178236+kellyfayrishta@users.noreply.github.com>
2022-12-11 15:31:28 -0500

    finished project

--- index.js
+++ index.js
@@ -1,13 +1,100 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "username",
+    message: "What is your Github username?",
+  },
+
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project name?",
+  },
+
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project: ",
+  },
+
+  {
+    type: "confirm",
+    name: "confirmLicenses",
+    message: "Would you like to include a license?",
+    default: false,
+  },
+
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have? (Use arrow keys)",
+    choices: ["MIT", "GPL", "CC--0", "None"],
+  },
+
+  {
+    type: "input",
+    name: "install",
+    message: "What command should be run to install dependencies?",
+    default: "npm i",
+  },
+
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+    default: "npm test",
+  },
+
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile("./README.md", data, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true,
+        message: console.log("Success!"),
+      });
+    });
+  });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+
+function init() {
+  return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+
+init()
+  .then((userInput) => {
+    return generateMarkdown(userInput);
+  })
+  .then((readmeInfo) => {
+    return writeToFile(readmeInfo);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,88 @@
+const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return `https://lbesson.mit-license.org/`
+  }
+  if (license === "GPL") {
+    return `http://perso.crans.org/besson/LICENSE.html`
+  }
+  if (license === "CC--0") {
+    return `https://creativecommons.org/licenses/by-nd/4.0`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `## Licenses
+    This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+  function generateMarkdown (data) {
+  return `
+  # ${data.title}
 
+  ${renderLicenseBadge(data.licenses)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Tests](#tests)
+  - [Questions](#Questions)
+
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+    ${data.install}
+
+  ## Usage
+
+    ${data.usage}
+
+  ## License
+
+    ${renderLicenseSection(data.license)}
+
+  ## Contributing
+
+    ${data.contributing}
+
+  ## Tests
+
+  To run tests, run the following command:
+
+    ${data.test}
+
+  ## Questions
+  
+  If you have any questions about the repo, open and issue or contact me directly at ${data.email}. You can find more of my work at [${data.username}](https://github.com/${data.username}.)
 `;
 }
 
